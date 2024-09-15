7295fd89eefb7d3f56e65bc615830b11292e667b
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-09-15 14:41:11 +0500

    ADDED FILES

---
+++ README.md
@@ -0,0 +1,2 @@
+# HACKATHON-1  | RESUME BUILDER
+ This Hackathon consists  of 5 milestone for making a dynamic resume builder
---
+++ script.ts
@@ -0,0 +1,51 @@
+const skills = document.getElementById('skills') as HTMLDivElement
+const skillButton = document.getElementById('btnS')
+const expform = document.getElementById('expform')
+const addexp = document.getElementById('addexp')
+const addskills = document.getElementById('addskills')
+const skillform = document.getElementById('skillform')
+const formInEdu = document.getElementById('formInEdu')
+const addedu = document.getElementById('addedu')
+
+
+if (skillButton) {
+    skillButton.addEventListener('click', () => {
+        skills.classList.toggle("hidden")
+        if (skills.classList.contains('hidden')) {
+            skillButton.textContent = 'SHOW SKILLS'
+        } else {
+            skillButton.textContent = 'HIDE SKILLS'
+        }
+    })
+} else {
+    console.error('something bad happened!')
+}
+
+if(addexp){
+    addexp.addEventListener('click',()=>{
+        if(expform){
+            const expform2 = expform.cloneNode(true)
+            expform.appendChild(expform2)
+        }
+    })
+}
+
+
+if(addedu){
+    addedu.addEventListener('click',()=>{
+        if(formInEdu){
+            const formInEdu2 = formInEdu.cloneNode(true)
+            formInEdu.appendChild(formInEdu2)
+        }
+    })
+}
+
+
+if(addskills){
+    addskills.addEventListener('click',()=>{
+        if(skillform){
+            const skillform2 = skillform.cloneNode(true)
+            skillform.appendChild(skillform2)
+        }
+    })
+}
